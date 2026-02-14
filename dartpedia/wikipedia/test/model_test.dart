c2002dd04619d1aa43e0722364b7ff7f98ac2413
Mate Narh <spartaninzaghi@gmail.com>
2026-02-14 15:13:41 -0500

    CH11 Mastered: added the test package as a dev dependency, wrote structured tests with the test package and matchers, and tested JSON deserialization logic

--- dartpedia/wikipedia/test/model_test.dart
+++ dartpedia/wikipedia/test/model_test.dart
@@ -13,13 +13,45 @@ const String openSearchResponse = './test/test_data/open_search_response.json';
 void main() {
   group('deserialize example JSON responses from wikipedia API', () {
     test('deserialize Dart Programming Language page summary example data from '
-        'json file into a Summary object', () async {
+          'json file into a Summary object', () async {
       final String pageSummaryInput =
           await File(dartLangSummaryJson).readAsString();
       final Map<String, Object?> pageSummaryMap =
           jsonDecode(pageSummaryInput) as Map<String, Object?>;
       final Summary summary = Summary.fromJson(pageSummaryMap);
       expect(summary.titles.canonical, 'Dart_(programming_language)');
+    });
+
+    test('deserialize Cat article example data from json file into '
+          'an Article object', () async {
+      final String articleJson = await File(catExtractJson).readAsString();
+      final Map<String, Object?> articleMap =
+          jsonDecode(articleJson) as Map<String, Object?>;
+
+      final Map<String, Object?> pagesMap =
+          (articleMap['query'] as Map)['pages'] as Map<String, Object?>;
+
+      // The 'pagesMap' contains a single key (e.g., '6678').
+      // We get the first (and only) value from that map.
+      final Map<String, Object?> catArticleMap =
+          pagesMap.values.first as Map<String, Object?>;
+
+      final Article article = Article(
+        title: catArticleMap['title'] as String,
+        extract: catArticleMap['extract'] as String,
+      );
+
+      expect(article.title.toLowerCase(), 'cat');
+    });
+
+    test('deserialize Open Search results example data from json file '
+        'into an SearchResults object', () async {
+      final String resultsString =
+          await File(openSearchResponse).readAsString();
+      final List<Object?> resultsAsList =
+          jsonDecode(resultsString) as List<Object?>;
+      final SearchResults results = SearchResults.fromJson(resultsAsList);
+      expect(results.results.length, greaterThan(1));
     });
   });
 }
