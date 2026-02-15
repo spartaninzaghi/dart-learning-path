bd0d9e38c49388532f8e66af5af7da9fa690ecf0
Mate Narh <spartaninzaghi@gmail.com>
2026-02-14 23:59:27 -0500

    [Final] CH13 Mastered: integrated logging into CLI commands, configuring log levels and error file outputs. This concludes the Dart Tutorial on dart.dev

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -1,20 +1,28 @@
+import 'package:cli/cli.dart';
 import 'package:command_runner/command_runner.dart';
 
-const version = '0.0.1';
+void main(List<String> arguments) async {
+  final errorLogger = initFileLogger('errors');
+  final app =
+      CommandRunner(
+          onOutput: (String output) async {
+            await write(output);
+          },
+          onError: (Object error) {
+            if (error is Error) {
+              errorLogger.severe(
+                '[Error] ${error.toString()}\n${error.stackTrace}',
+              );
+              throw error;
+            }
+            if (error is Exception) {
+              errorLogger.warning(error);
+            }
+          },
+        )
+        ..addCommand(HelpCommand())
+        ..addCommand(SearchCommand(logger: errorLogger))
+        ..addCommand(GetArticleCommand(logger: errorLogger));
 
-void main(List<String> arguments) {
-  var commandRunner = CommandRunner(
-    onOutput: (String output) async {
-      await write(output);
-    },
-    onError: (Object error) {
-      if (error is Error) {
-        throw error;
-      }
-      if (error is Exception) {
-        print(error);
-      }
-    },
-  )..addCommand(HelpCommand());
-  commandRunner.run(arguments);
+  app.run(arguments);
 }
